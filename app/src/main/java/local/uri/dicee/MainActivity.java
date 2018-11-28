a9a2096be99f7c1f15dccd79b92a129ca8080a9f
Yuri Strelnikov <yuri.strelnikov@mail.ru>
2018-11-28 11:07:27 +0300

    App is finished, enjoy :)

--- app/src/main/java/local/uri/dicee/MainActivity.java
+++ app/src/main/java/local/uri/dicee/MainActivity.java
@@ -1,17 +1,21 @@
 package local.uri.dicee;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Button rollButton;
     ImageView left_dice;
     ImageView right_dice;
+
+    final int[] diceArray = {R.drawable.dice1, R.drawable.dice2, R.drawable.dice3,
+            R.drawable.dice4, R.drawable.dice5, R.drawable.dice6};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +24,19 @@ public class MainActivity extends AppCompatActivity {
 
         rollButton = findViewById(R.id.rollButton);
         left_dice = findViewById(R.id.image_leftDice);
-        left_dice = findViewById(R.id.image_rightDice);
+        right_dice = findViewById(R.id.image_rightDice);
 
         rollButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                Log.d("Dicee", "Ze dicee log message!");
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(6);
+                left_dice.setImageResource(diceArray[number]);
+                number = randomNumberGenerator.nextInt(6);
+                right_dice.setImageResource(diceArray[number]);
             }
+
         });
     }
 }
